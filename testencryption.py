c80a477afefb745a80078c8c85fb8ce3d0bfddcf
claudeb <urzawrym@hotmail.com>
2021-03-17 15:47:47 -0400

    ok

---
+++ testencryption.py
@@ -0,0 +1,17 @@
+"""from cryptography.fernet import Fernet
+# Put this somewhere safe!
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"Hello")
+print(token)
+
+print(f.decrypt(token))"""
+
+import base64
+
+
+print (base64.b64encode(b"Hello"))
+
+
+
+

eb9c9810f1bf3aca0d99afef1289f8f3a33fb537
claudeb <urzawrym@hotmail.com>
2021-04-06 12:04:03 -0400

    ok

--- testtooltip.py
+++ testtooltip.py
@@ -6,31 +6,7 @@ import sys
 
 
 data_for_tree = {"tomato":{"color":"red","amount":"10", "note":"a note for tomato"},"banana":{"color":"yellow","amount":"1", "note":"b note for banana"}, "some fruit":{"color":"unknown","amount":"100", "note":"some text"}}
-
-"""class TreeModel(QAbstractItemModel):
-
-    def data(self, index, role=Qt.DisplayRole):
-        #...
-        if role == Qt.ToolTipRole:
-            return 'ToolTip'
-
-    def flags(self, index):
-        if not index.isValid():
-            return Qt.NoItemFlags # 0
-        return Qt.ItemIsSelectable # or Qt.ItemIsEnabled
-
-class ProxyModel(QSortFilterProxyModel):
-
-    def __init__(self, parent=None):
-        super(ProxyModel, self).__init__(parent)
-
-    def lessThan(self, left, right):
-        leftData = self.sourceModel().data(left)
-        rightData = self.sourceModel().data(right)
-        try:
-            return float(leftData) < float(rightData)
-        except ValueError:
-            return leftData < rightData"""
+datatest = [{"nom": "Administrateur", "prenom": "Admin", "sexe": "Masculin", "dateinscription": "01-01-0000", "courriel": "admin", "password": "admin123", "cartes": [{"numero": "12345678", "expiration": "12/04/03", "codecarte": "005"}]}]
 
 class MainFrame(QWidget):
     def __init__(self):
@@ -39,10 +15,11 @@ class MainFrame(QWidget):
         self.MyTreeView = QTreeView()
         self.MyTreeViewModel = QStandardItemModel()
         self.MyTreeView.setModel(self.MyTreeViewModel)
-        self.most_used_cat_header = ['Name', "amount", "color"]
+        self.most_used_cat_header = ['Nom', "Prénom", "Sexe", "Date Inscription", "Courriel Client", "Mot de passe"]
         self.MyTreeViewModel.setHorizontalHeaderLabels(self.most_used_cat_header)
         self.MyTreeView.setSortingEnabled(True)
-        self.MyTreeView_Fill()
+        #self.MyTreeView_Fill()
+        self.Testdata()
 
         MainWindow = QHBoxLayout(self)
         MainWindow.addWidget(self.MyTreeView)
@@ -61,11 +38,30 @@ class MainFrame(QWidget):
             self.MyTreeViewModel.appendRow(item)
         #self.MyTreeView.sortByColumn(1, Qt.DescendingOrder)
 
+    def Testdata(self):
+        for k in datatest:
+            nom = QStandardItem(k["nom"])
+            prenom = QStandardItem(k["prenom"])
+            sexe = QStandardItem(k["sexe"])
+            date = QStandardItem(k["dateinscription"])
+            courriel = QStandardItem(k["courriel"])
+            password = QStandardItem(k["password"])
+            #cartes = QStandardItem(k["cartes"])
+            tooltip = datatest(k["cartes"])
+            nom.setToolTip(tooltip)
+            prenom.setToolTip(tooltip)
+            sexe.setToolTip(tooltip)
+            date.setToolTip(tooltip)
+            courriel.setToolTip(tooltip)
+            password.setToolTip(tooltip)
+            item = (nom, prenom, sexe, date, courriel, password)
+            self.MyTreeViewModel.appendRow(item)
 
-        c = 0
+
+        """c = 0
         while c < len(self.most_used_cat_header):
             self.MyTreeView.resizeColumnToContents(c)
-            c=c+1
+            c=c+1"""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

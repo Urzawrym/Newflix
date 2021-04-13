72624425bfd55145f7704fc39c8f75366d503313
Claude-PC <urzawrym@hotmail.com>
2021-04-13 14:07:42 -0400

    ok

--- newflix.py
+++ newflix.py
@@ -419,41 +419,26 @@ class Controller: #C'est dans cette classe que l'action se passe, toutes les mod
     def modifcustomer(self, index):
         self.donneesclient = self.mainw.treeView.selectedIndexes()[0]
         if self.donneesclient.data() == "*****" :
-            print(self.donneesclient.parent().data())
-        else : print(self.donneesclient.data())
+            self.donneesclient = self.donneesclient.parent()
 
-
-
-
-
-
-
-        """if self.donneesclient == [] :
-            msg = QtWidgets.QMessageBox()
-            msg.setIcon(QtWidgets.QMessageBox.Warning)
-            msg.setText("Veuillez choisir un client à modifier")
-            msg.setInformativeText('')
-            msg.setWindowTitle("Erreur")
-            msg.exec_()
-        else:
-            self.popupcustomer = FormClient()
-            self.popupcustomer.show()
-            #self.popupcustomer.pushButton.clicked.connect(self.modifclient)
-            self.popupcustomer.pushButton_2.clicked.connect(self.popupcustomer.close)
-            index = self.popupcustomer.comboBox.findText(self.donneesclient[2], QtCore.Qt.MatchFlag.MatchFixedString)
-            date = QtCore.QDate.fromString(self.donneesclient[3], "dd-MM-yyyy")
-            self.model3 = QtGui.QStandardItemModel()
-            self.model3.setHorizontalHeaderLabels(['Numéro de carte', 'Date Expiration', 'Code Carte'])
-            self.popupcustomer.treeView.setModel(self.model3)  # Active le modèle
-            #for dict in
-            self.updateclient = Client(
-                self.popupcustomer.lineEdit.setText(self.donneesclient[0]),
-                self.popupcustomer.lineEdit_2.setText(self.donneesclient[1]),
-                self.popupcustomer.comboBox.setCurrentIndex(index),
-                self.popupcustomer.dateEdit.setDate(date),
-                self.popupcustomer.lineEdit_3.setText(self.donneesclient[4]),
-                self.popupcustomer.lineEdit_5.setText(self.donneesclient[5]),
-            ["test1, test2, test3"])"""
+        self.popupcustomer = FormClient()
+        self.popupcustomer.show()
+        #self.popupcustomer.pushButton.clicked.connect(self.modifclient)
+        self.popupcustomer.pushButton_2.clicked.connect(self.popupcustomer.close)
+        index = self.popupcustomer.comboBox.findText(self.donneesclient[2], QtCore.Qt.MatchFlag.MatchFixedString)
+        date = QtCore.QDate.fromString(self.donneesclient[3], "dd-MM-yyyy")
+        self.model3 = QtGui.QStandardItemModel()
+        self.model3.setHorizontalHeaderLabels(['Numéro de carte', 'Date Expiration', 'Code Carte'])
+        self.popupcustomer.treeView.setModel(self.model3)  # Active le modèle
+        #for dict in
+        self.updateclient = Client(
+            self.popupcustomer.lineEdit.setText(self.donneesclient[0]),
+            self.popupcustomer.lineEdit_2.setText(self.donneesclient[1]),
+            self.popupcustomer.comboBox.setCurrentIndex(index),
+            self.popupcustomer.dateEdit.setDate(date),
+            self.popupcustomer.lineEdit_3.setText(self.donneesclient[4]),
+            self.popupcustomer.lineEdit_5.setText(self.donneesclient[5]),
+            ["test1, test2, test3"])
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
